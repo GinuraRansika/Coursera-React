49ab17c1d9045b13a2fe12f872e1b7fb64009520
Ginura Ransika De Seram <ginuraransika@gmail.com>
2022-08-04 11:52:17 +0530

    Finished the Components Part 2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import React from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
-import './App.css';
+import React, { Component } from "react";
+import { Container, Navbar, NavbarBrand } from "reactstrap";
+import Menu from "./components/MenuComponent";
+import "./App.css";
+import { DISHES } from "./shared/dishes";
 
-function App() {
-  return (
-    <div >
-      <Navbar dark color='primary'>
-        <container  fluid>
-          <NavbarBrand href='/' className='ms-5 '>
-            Ristorante Con Fusion
-          </NavbarBrand>
-        </container>
-      </Navbar>
-      <Menu/>
-    </div>
-  );
+class App extends Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         dishes: DISHES,
+      };
+   }
+
+   render() {
+      return (
+         <div>
+            <Navbar dark color="primary">
+               <Container fluid>
+                  <NavbarBrand href="/" className="ms-5 ">
+                     Ristorante Con Fusion
+                  </NavbarBrand>
+               </Container>
+            </Navbar>
+            <Menu dishes={this.state.dishes} />
+         </div>
+      );
+   }
 }
 
 export default App;
